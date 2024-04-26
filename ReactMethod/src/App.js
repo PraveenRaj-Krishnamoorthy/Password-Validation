4c295cbe596de2e9c725ce3a2d949e84b875250d
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-04-26 18:17:48 +0530

    ReactJS Completed
    Css to be completed

--- ReactMethod/src/App.js
+++ ReactMethod/src/App.js
@@ -1,4 +1,5 @@
 import Password from "./Components/Password";
+import './App.css'
 
 export default function App() {
   return(
--- ReactMethod/src/Components/Password.jsx
+++ ReactMethod/src/Components/Password.jsx
@@ -1,34 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Password() {
-  const [pass, setPass] = useState("");
-  const [eight, setEight] = useState("Eight");
-  const [upp, setUpp] = useState("Upp");
-  const [low, setLow] = useState("Low");
-  const [num, setNum] = useState("Num");
-  const [spl, setSpl] = useState("Spl");
-  
-  const verify = (e) => {
-    let received = e.target.value;
-    setPass(received);
-    if (pass.length>=7) {
-        
-    }
-  };
+  const [Password, setPassword] = useState("")
+  const [color, setColor] = useState({
+    svn: "red",
+    upp: "red",
+    low: "red",
+    num: "red",
+    spl: "red"
+  })
 
+  useEffect(() => {
+    setColor({
+      svn: Password.length > 6 ? "green" : "red",
+      upp: /[A-Z]/g.test(Password) ? "green" : "red",
+      low: /[a-z]/g.test(Password) ? "green" : "red",
+      num: /[0-1]/g.test(Password) ? "green" : "red",
+      spl: /[!@#$%^&*]/g.test(Password) ? "green" : "red"
+    })
+  }, [Password])
   return (
     <>
-      <input type="password" onChange={verify} />
-      <h1>Password {pass}</h1>
-      <div className="condition">
-        <ul>
-          <li value={eight}>Eight</li>
-          <li value={upp}>Upp</li>
-          <li value={low}>Low</li>
-          <li value={num}>Num</li>
-          <li value={spl}>Spl</li>
-        </ul>
+      <div className="container">
+        <main>
+          <input type="password" onChange={(e) => { setPassword(e.target.value) }} />
+          <ul>
+            <h1 style={{ color: color.svn }}>Contains Seven Or More</h1>
+            <h1 style={{ color: color.upp }}>Contains Upper</h1>
+            <h1 style={{ color: color.low }}>Contains Lower</h1>
+            <h1 style={{ color: color.num }}>Containss Number</h1>
+            <h1 style={{ color: color.spl }}>Contains Special</h1>
+          </ul>
+        </main>
+        <div className="output">
+          <h1>{Password}</h1>
+        </div>
       </div>
     </>
-  );
+  )
 }
