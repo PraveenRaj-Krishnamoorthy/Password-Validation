5998d730bc7d188902071d83f34ffa1ea32b198a
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-04-28 21:01:44 +0530

    updates
    updates

--- ReactMethod/src/Components/Password.jsx
+++ ReactMethod/src/Components/Password.jsx
@@ -1,29 +1,34 @@
 import { useEffect, useState } from "react";
 
 export default function Password() {
-  const [Password, setPassword] = useState("")
+  const [Password, setPassword] = useState("");
   const [color, setColor] = useState({
-    svn: "red",
-    upp: "red",
-    low: "red",
-    num: "red",
-    spl: "red"
-  })
+    svn: "black",
+    upp: "black",
+    low: "black",
+    num: "black",
+    spl: "black",
+  });
 
   useEffect(() => {
     setColor({
       svn: Password.length > 6 ? "green" : "red",
       upp: /[A-Z]/g.test(Password) ? "green" : "red",
       low: /[a-z]/g.test(Password) ? "green" : "red",
-      num: /[0-1]/g.test(Password) ? "green" : "red",
-      spl: /[!@#$%^&*]/g.test(Password) ? "green" : "red"
-    })
-  }, [Password])
+      num: /[0-9]/g.test(Password) ? "green" : "red",
+      spl: /[!@#$%^&*]/g.test(Password) ? "green" : "red",
+    });
+  }, [Password]);
   return (
     <>
       <div className="container">
         <main>
-          <input type="password" onChange={(e) => { setPassword(e.target.value) }} />
+          <input
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
           <ul>
             <h1 style={{ color: color.svn }}>Contains Seven Or More</h1>
             <h1 style={{ color: color.upp }}>Contains Upper</h1>
@@ -37,5 +42,5 @@ export default function Password() {
         </div>
       </div>
     </>
-  )
+  );
 }
